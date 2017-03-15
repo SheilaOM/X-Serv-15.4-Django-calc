7b67b41b23bd4a10a22d88abf65971838cba2cb8
SheilaOM <sheila.oliva14@gmail.com>
2017-03-15 19:13:30 +0100

    urls terminado

--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from calc import views
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'(\d+)([\+\-\*/])(\d+)', views.calcula, name="calcula"),
+    url(r'(.*)', views.notfound, name="notfound"),
 )

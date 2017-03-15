b13dbc72b1ae60560a39b663dd1080fe4a694483
SheilaOM <sheila.oliva14@gmail.com>
2017-03-15 19:14:16 +0100

    views terminado

--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def calcula(request, sumando1, operador, sumando2):
+    if operador == "+":
+        res = int(sumando1)+int(sumando2)
+    elif operador == "-":
+        res = int(sumando1)-int(sumando2)
+    elif operador == "*":
+        res = int(sumando1)*int(sumando2)
+    elif operador == "/":
+        try:
+            res = int(sumando1)/int(sumando2)
+        except ZeroDivisionError:
+            return HttpResponse('<h1>Quiero hacer ' +
+                                sumando1 + operador + sumando2 +
+                                '. Error: división entre 0</h1>')
+
+    return HttpResponse('<h1><body>Calculadora</h1><p><b>' +
+                        sumando1 + operador + sumando2 +
+                        ' = ' + str(res) + '</b></p></body>')
+
+def notfound(request, parametro):
+    return HttpResponse('<h1>Not Found</h1>')
